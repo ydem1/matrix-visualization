0eb73211b9b008535a92222d7424334f7fa3a421
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2025-01-31 15:19:10 +0200

    add plus and minus buttons

--- src/components/Table/TableCell/index.tsx
+++ src/components/Table/TableCell/index.tsx
@@ -6,5 +6,27 @@ interface Props {
 }
 
 export const TableCell: React.FC<Props> = ({ cell }) => {
-  return <td className={styles.cell}>{cell.amount}</td>;
+  const handlePlusButton = () => {
+    console.log("handlePlusButton");
+    console.log(cell);
+  };
+
+  const handleMinusButton = () => {
+    console.log("handleMinusButton");
+    console.log(cell);
+  };
+
+  return (
+    <td className={styles.cell}>
+      <div className={styles["cell__wrapper"]}>
+        <button className={styles["cell__button"]} onClick={handlePlusButton}>
+          +
+        </button>
+        <span>{cell.amount}</span>
+        <button className={styles["cell__button"]} onClick={handleMinusButton}>
+          -
+        </button>
+      </div>
+    </td>
+  );
 };
